c22056e6ab322b60a0eb46b0b9ca97ba96a6046c
Paul Strentz <strentz.paul@gmail.com>
2020-05-09 22:40:09 +0200

    Regex is too complicated for the moment

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,10 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class WildController
+ * @Route("/wild", name="wild_")
+ */
+
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() : Response
     {
@@ -17,4 +22,33 @@ Class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+
+        /**
+         * @Route("/show/{slug}", name="show",requirements={"slug"="[a-z0-9\-]+"})
+         *
+         */
+        public function show($slug): Response
+    {
+        $slugClean = str_replace('-',' ',$slug);
+       $slugCleanCapit = ucwords($slugClean);
+        return $this->render('wild/show.html.twig', ['slug' => $slugCleanCapit]);
+
+    }
+
+//    /**
+//     * @Route("/show/{slug}",
+//     *     methods={"GET"},
+//     *     requirements={"slug"="[0-9]"},
+//     *     defaults={"slug"="Aucune-série-sélectionnée,-veuillez-choisir-une-série"},
+//     *     name="show"
+//     * )
+//     */
+//    public function show(string $slug): Response
+//    {
+//       $slugClean = str_replace('-',' ',$slug);
+//       $slugCleanCapit = ucwords($slugClean);
+//        return $this->render('wild/show.html.twig', ['slug' => $slugCleanCapit]);
+//    }
+
 }
