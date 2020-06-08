21ea4106120d0b96490e75349368e6a3a57e80cc
Paul Strentz <strentz.paul@gmail.com>
2020-06-08 16:09:42 +0200

    Quete 18 fini elle est rudement longue pour la mise en page

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,6 +9,8 @@ use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,6 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProgramController extends AbstractController
 {
+    const SENDEREMAIL =  "strentz.paul@gmail.com";
     /**
      * @Route("/", name="program_index", methods={"GET"})
      */
@@ -28,8 +31,13 @@ class ProgramController extends AbstractController
 
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
+     * @param Request $request
+     * @param Slugify $slugify
+     * @param Mailer $mailer
+     * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -41,6 +49,13 @@ class ProgramController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($program);
             $entityManager->flush();
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to($this->getParameter('mailer_to'))
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('program/email/notification.html.twig', ['name'=>$program, 'actors'=>$program->getActors()]));
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('program_index');
         }
